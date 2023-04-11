427421b5ba99407cc5b5a6f14209e27337f43381
Kavindu Fernando <kavinduf2@gmail.com>
2023-04-11 18:35:52 +0530

    update

---
+++ Components/CatergoryCard.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Button, Card } from "@rneui/themed";
+
+const CatergoryCard = ({ onClick, title }) => {
+  return (
+    <View>
+      <Card></Card>
+    </View>
+  );
+};
+
+export default CatergoryCard;
+
+const styles = StyleSheet.create({});
